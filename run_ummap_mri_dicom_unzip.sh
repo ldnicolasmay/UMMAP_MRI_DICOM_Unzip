fde5e3b8f5ac3567d5435446e74773a02dce7585
L. D. Nicolas May <LDNicolasMay@gmail.com>
2020-04-28 16:40:50 -0400

    simplify bash script date prints

--- run_ummap_mri_dicom_unzip.sh
+++ run_ummap_mri_dicom_unzip.sh
@@ -1,9 +1,8 @@
-echo $(date)
+date
 
 python3 ummap_mri_dicom_unzip.py                     \
   --mri_path /nfs/psych-bhampstelab/RAW_nopreprocess \
   --subfolder_regex "^(hlp|bmh)17umm\d{5}_\d{5}$"    \
   --verbose
 
-echo $(date)
-
+date
